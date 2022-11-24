e6fcfa71bf780d9f3eb0d4163a6a9007271e2460
hcamusso <hcamusso@gmail.com>
2022-11-24 17:03:02 -0300

    Prueba del test que falla y ahora se arregla

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function mooncakeSays(message) {
-  // let mooncake = '(o.o)';
+  let mooncake = '(o.o)';
 
-  // if ( !message ) {
-  //   return `${mooncake} chookity?`;
-  // }
+  if ( !message ) {
+    return `${mooncake} chookity?`;
+  }
 
   return `${mooncake} ${message}`;
 }
